327d756229e72e477d9a249948de5a1e723ecd17
牛亮 <nll@itcast.cn>
2019-08-09 15:04:28 +0800

    点击按钮验证，重构代码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -75,23 +75,22 @@ export default {
     async handleLogin () {
       try {
         // 进行表单验证
-        this.$validator.validate().then(async valid => {
-          if (!valid) {
-            // do stuff if not valid.
-            return
-          }
-          const data = await login(this.user)
-          // 记录登录状态
-          // 1. 把登录状态记录到 localStorage
-          // window.localStorage.setItem('user', JSON.stringify(data))
-          // 2. 把登录状态记录到 vuex，store中
+        const valid = await this.$validator.validate()
+        if (!valid) {
+          // 验证失败，返回
+          return
+        }
+        const data = await login(this.user)
+        // 记录登录状态
+        // 1. 把登录状态记录到 localStorage
+        // window.localStorage.setItem('user', JSON.stringify(data))
+        // 2. 把登录状态记录到 vuex，store中
 
-          // 提交 mutation,记录到state中
-          this.$store.commit('setUser', data)
-          // 跳转到首页
-          this.$router.push({
-            name: 'home'
-          })
+        // 提交 mutation,记录到state中
+        this.$store.commit('setUser', data)
+        // 跳转到首页
+        this.$router.push({
+          name: 'home'
         })
 
       } catch (err) {
