15848bf853a4adf5b4e130ba002b8a06d0c984d8
牛亮 <nll@itcast.cn>
2019-08-09 17:16:26 +0800

    封装获取文章列表的api

---
+++ src/api/article.js
@@ -0,0 +1,18 @@
+import request from '@/utiles/request'
+
+/**
+ * 获取文章数据
+ */
+export const getArticles = ({
+  channelId,
+  timestamp,
+  withTop = 1
+}) => {
+  return request.get('/app/v1_1/articles', {
+    params: {
+      channel_id: channelId,
+      timestamp: timestamp,
+      with_top: withTop
+    }
+  })
+}
