a9b02119249862fa47557efbc0892ae9a5c8ae7c
牛亮 <nll@itcast.cn>
2019-08-09 15:49:41 +0800

    导航头和标签页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    home
+    <!-- 导航栏 -->
+    <van-nav-bar
+      title="黑马头条"
+    />
+    <!-- 频道列表 -->
+    <van-tabs>
+      <van-tab v-for="index in 8" :title="'标签 ' + index" :key="index">
+        内容 {{ index }}
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
