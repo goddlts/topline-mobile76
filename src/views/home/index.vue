8a7a0800c24e2454a1359583aa7146c3b46e9d11
牛亮 <nll@itcast.cn>
2019-08-09 16:06:48 +0800

    下拉加载组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,26 +4,29 @@
     <van-nav-bar
       title="黑马头条"
     />
-    <!-- 频道列表 -->
-    <van-tabs>
-      <van-tab v-for="index in 8" :title="'标签 ' + index" :key="index">
-        <!-- 文章列表，不同的频道有不同的文章列表 -->
-        <!-- 当list没有到达页面底部的时候，会触发load事件，并且把loading设置为true -->
-        <!-- loading 为true 会显示正在加载... ，当数据加载完毕应该把loading设置为false -->
-        <van-list
-          v-model="loading"
-          :finished="finished"
-          finished-text="没有更多了"
-          @load="onLoad"
-        >
-          <van-cell
-            v-for="item in list"
-            :key="item"
-            :title="item"
-          />
-        </van-list>
-      </van-tab>
-    </van-tabs>
+    <!-- 下拉加载最新数据 -->
+    <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+      <!-- 频道列表 -->
+      <van-tabs>
+        <van-tab v-for="index in 8" :title="'标签 ' + index" :key="index">
+          <!-- 文章列表，不同的频道有不同的文章列表 -->
+          <!-- 当list没有到达页面底部的时候，会触发load事件，并且把loading设置为true -->
+          <!-- loading 为true 会显示正在加载... ，当数据加载完毕应该把loading设置为false -->
+          <van-list
+            v-model="loading"
+            :finished="finished"
+            finished-text="没有更多了"
+            @load="onLoad"
+          >
+            <van-cell
+              v-for="item in list"
+              :key="item"
+              :title="item"
+            />
+          </van-list>
+        </van-tab>
+      </van-tabs>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -34,11 +37,14 @@ export default {
       // list，文章列表的数据
       list: [],
       loading: false,
-      finished: false
+      finished: false,
+      // 下拉更新组件的
+      isLoading: false
     };
   },
 
   methods: {
+    // list组件
     onLoad() {
       // 异步更新数据
       setTimeout(() => {
@@ -52,6 +58,14 @@ export default {
         if (this.list.length >= 40) {
           this.finished = true;
         }
+      }, 500);
+    },
+    // 下拉加载更多组件
+    onRefresh() {
+      setTimeout(() => {
+        this.$toast('刷新成功');
+        this.isLoading = false;
+        this.count++;
       }, 500);
     }
   }
