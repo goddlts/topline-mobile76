e84bcd89f63590aceb1fd6f532095e671102ccde
牛亮 <nll@itcast.cn>
2019-08-09 17:02:44 +0800

    频道列表

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -9,7 +9,10 @@
     <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
       <!-- 频道列表 -->
       <van-tabs>
-        <van-tab v-for="index in 8" :title="'标签 ' + index" :key="index">
+        <van-tab
+          v-for="channel in channels"
+          :title="channel.name"
+          :key="channel.id">
           <!-- 文章列表，不同的频道有不同的文章列表 -->
           <!-- 当list没有到达页面底部的时候，会触发load事件，并且把loading设置为true -->
           <!-- loading 为true 会显示正在加载... ，当数据加载完毕应该把loading设置为false -->
@@ -32,6 +35,7 @@
 </template>
 
 <script>
+import { getChannels } from '@/api/channel'
 export default {
   data() {
     return {
@@ -40,11 +44,40 @@ export default {
       loading: false,
       finished: false,
       // 下拉更新组件的
-      isLoading: false
+      isLoading: false,
+      // 存储频道列表
+      channels: []
     };
   },
 
+  created () {
+    // 获取频道数据
+    this.loadChannels()
+  },
+
   methods: {
+    // 获取频道数据
+    async loadChannels () {
+      // 测试
+      // const data = await getChannels()
+      // console.log(data)
+
+      // 1. 判断用户是否登录，如果用户登录，直接获取数据
+      if (this.$store.state.user) {
+        const data = await getChannels()
+        this.channels = data.channels
+      } else {
+        // 2. 如果用户没有登录，从本地缓存中获取数据
+        this.channels = JSON.parse(window.localStorage.getItem('channels')) || []
+        // 3. 如果本地缓存没有数据，发送请求获取数据，存储到本地存储中
+        if (this.channels.length === 0) {
+          const data = await getChannels()
+          this.channels = data.channels
+          // 存储到本地存储中
+          window.localStorage.setItem('channels', JSON.stringify(this.channels))
+        }
+      }
+    },
     // list组件
     onLoad() {
       // 异步更新数据
