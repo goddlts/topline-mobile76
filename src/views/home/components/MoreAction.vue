36d2f4cff49557a3e98d25d8fffe238ed9dddca0
牛亮 <nll@itcast.cn>
2019-08-11 18:07:13 +0800

    举报文章-优化

--- src/views/home/components/MoreAction.vue
+++ src/views/home/components/MoreAction.vue
@@ -19,9 +19,13 @@
     <van-cell-group v-show="isReportShow">
       <van-cell icon="arrow-left" @click="isReportShow=false" />
       <!-- // 举报类型： 0-其他问题，1-标题夸张，2-低俗色情，3-错别字多，4-旧闻重复，5-广告软文，6-内容不实，7-涉嫌违法犯罪，8-侵权'	 -->
-      <van-cell title="标题夸张" @click="handle('report', 1)" />
-      <van-cell title="低俗色情" @click="handle('report', 2)" />
-      <van-cell title="错别字多" @click="handle('report', 3)" />
+      <van-cell
+        v-for="item in list"
+        :key="item.type"
+        :title="item.title"
+        @click="handle('report', item.type)" />
+      <!-- <van-cell title="低俗色情" @click="handle('report', 2)" />
+      <van-cell title="错别字多" @click="handle('report', 3)" /> -->
     </van-cell-group>
   </van-dialog>
 </template>
@@ -36,7 +40,19 @@ export default {
   data () {
     return {
       // 控制反馈垃圾内容的cell是否显示
-      isReportShow: false
+      isReportShow: false,
+      // 存储所有的反馈类型
+      list: [
+        { title: '标题夸张', type: 1 },
+        { title: '低俗色情', type: 2 },
+        { title: '错别字多', type: 3 },
+        { title: '旧闻重复', type: 4 },
+        { title: '广告软文', type: 5 },
+        { title: '内容不实', type: 6 },
+        { title: '涉嫌违法犯罪', type: 7 },
+        { title: '侵权', type: 8 },
+        { title: '其它', type: 0 }
+      ]
     }
   },
   methods: {
