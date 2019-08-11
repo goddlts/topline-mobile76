5d38607ae49bd045e5ce7565940ca087023780d3
牛亮 <nll@itcast.cn>
2019-08-11 18:01:57 +0800

    举报文章

--- src/api/article.js
+++ src/api/article.js
@@ -28,3 +28,19 @@ export const dislikeArticle = (id) => {
     target: id
   })
 }
+
+/**
+ * 举报文章
+ * @param {} param0 
+ */
+export const reportArticle = ({
+  // 举报文章的id
+  id,
+  // 举报类型： 0-其他问题，1-标题夸张，2-低俗色情，3-错别字多，4-旧闻重复，5-广告软文，6-内容不实，7-涉嫌违法犯罪，8-侵权'	
+  type
+}) => {
+  return request.post('/app/v1_0/article/reports', {
+    target: id,
+    type: type
+  })
+}
--- src/views/home/components/MoreAction.vue
+++ src/views/home/components/MoreAction.vue
@@ -18,15 +18,16 @@
 
     <van-cell-group v-show="isReportShow">
       <van-cell icon="arrow-left" @click="isReportShow=false" />
-      <van-cell title="标题夸张" />
-      <van-cell title="低俗色情" />
-      <van-cell title="错别字多" />
+      <!-- // 举报类型： 0-其他问题，1-标题夸张，2-低俗色情，3-错别字多，4-旧闻重复，5-广告软文，6-内容不实，7-涉嫌违法犯罪，8-侵权'	 -->
+      <van-cell title="标题夸张" @click="handle('report', 1)" />
+      <van-cell title="低俗色情" @click="handle('report', 2)" />
+      <van-cell title="错别字多" @click="handle('report', 3)" />
     </van-cell-group>
   </van-dialog>
 </template>
 
 <script>
-import { dislikeArticle } from '@/api/article'
+import { dislikeArticle, reportArticle } from '@/api/article'
 import { blackUserList } from '@/api/user'
 
 export default {
@@ -40,13 +41,16 @@ export default {
   },
   methods: {
     // 点击cell的时候统一处理行为
-    handle (type) {
+    handle (type, reportType) {
       switch (type) {
         case 'dislike':
           this.dislike()
           break
         case 'blacklist':
           this.blacklist()
+          break
+        case 'report':
+          this.report(reportType)
           break
       }
     },
@@ -66,6 +70,7 @@ export default {
         this.$toast.fail('操作失败' + err)
       }
     },
+    // 拉黑作者
     async blacklist () {
       try {
         // 1. 获取到作者的id
@@ -76,6 +81,21 @@ export default {
         this.$toast.success('操作成功')
         // 4. 成功，通知home组件，隐藏MoreAction，删除数据
          this.$emit('handleSuccess')
+      } catch (err) {
+        this.$toast.fail('操作失败' + err)
+      }
+    },
+    // 举报文章
+    async report (reportType) {
+      try {
+        const id = this.currentArticle.art_id
+        await reportArticle({
+          id: id,
+          type: reportType
+        })
+        this.$toast.success('操作成功')
+        // 操作成功--通知home组件，隐藏MoreAction
+        this.$emit('input', false)
       } catch (err) {
         this.$toast.fail('操作失败' + err)
       }
