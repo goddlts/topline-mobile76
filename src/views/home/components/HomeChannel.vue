c9b045dff8b4983ff65f4d45f2a5479a4705418d
牛亮 <nll@itcast.cn>
2019-08-13 15:08:03 +0800

    频道管理-搭建页面结构

--- src/views/home/components/HomeChannel.vue
+++ src/views/home/components/HomeChannel.vue
@@ -5,7 +5,49 @@
     position="bottom"
     :style="{ height: '90%' }"
   >
-    HelloWorld
+    <div class="channel">
+      <div class="channel-head">
+        <div>
+          <span class="title">我的频道</span>
+          <span class="desc">点击进入频道</span>
+        </div>
+        <div>
+          <van-button
+            type="danger"
+            plain
+            size="mini"
+          >编辑</van-button>
+        </div>
+      </div>
+      <van-grid class="channel-content" :gutter="10" clickable>
+        <van-grid-item
+          v-for="value in 8"
+          :key="value"
+          text="文字">
+          <span class="text">文字</span>
+          <van-icon class="close-icon" name="close" />
+        </van-grid-item>
+      </van-grid>
+    </div>
+
+    <div class="channel">
+      <div class="channel-head">
+        <div>
+          <span class="title">频道推荐</span>
+          <span class="desc">点击添加频道</span>
+        </div>
+      </div>
+      <van-grid class="channel-content" :gutter="10" clickable>
+        <van-grid-item
+          v-for="value in 8"
+          :key="value"
+          text="文字">
+          <div class="info">
+            <span class="text">文字</span>
+          </div>
+        </van-grid-item>
+      </van-grid>
+    </div>
   </van-popup>
 </template>
 
@@ -21,6 +63,40 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.channel {
+  .channel-head {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    .title {
+      font-size: 30px;
+      margin-right: 5px;
+    }
+    .desc {
+      font-size: 12px;
+    }
+  }
+  .channel-content {
+    .text {
+      font-size: 16px;
+    }
+    .active {
+      color: red;
+    }
+    .close-icon {
+      font-size: 20px;
+      position: absolute;
+      top: -5px;
+      right: -5px;
+      z-index: 999;
+      background-color: #fff;
+    }
+    .info {
+      display: flex;
+      align-items: center;
+    }
+  }
+}
 </style>
--- src/views/layout-tabbar/index.vue
+++ src/views/layout-tabbar/index.vue
@@ -2,8 +2,8 @@
   <div>
     <router-view></router-view>
     <!-- tabbar -->
-    <van-tabbar v-model="active" route="">
-      <van-tabbar-item icon="home-o" to="/">主页</van-tabbar-item>
+    <van-tabbar v-model="active" route>
+      <van-tabbar-item icon="home-o" replace to="/">主页</van-tabbar-item>
       <van-tabbar-item icon="search" to="/qa">问答</van-tabbar-item>
       <van-tabbar-item icon="friends-o" to="/video">视频</van-tabbar-item>
       <van-tabbar-item icon="setting-o" to="/my">我的</van-tabbar-item>
