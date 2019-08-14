2e87fe04845b60343945484761cc427e8ddd7d6e
牛亮 <nll@itcast.cn>
2019-08-14 15:43:53 +0800

    搜索功能-搜索结果界面

--- src/views/search-result/index.vue
+++ src/views/search-result/index.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    搜索结果
+    <van-nav-bar
+      title="标题"
+      left-text="返回"
+      left-arrow
+      fixed
+      @click-left="$router.back()"
+    />
+    <!-- 列表 -->
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell
+        v-for="item in list"
+        :key="item"
+        :title="item"
+      />
+    </van-list>
   </div>
 </template>
 
@@ -8,13 +27,36 @@
 export default {
   name: 'SearchResult',
   props: ['q'],
-  created() {
-    // console.log(this.$route.params.q)
-    console.log(this.q)
+  data() {
+    return {
+      list: [],
+      loading: false,
+      finished: false
+    };
+  },
+
+  methods: {
+    onLoad() {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1);
+        }
+        // 加载状态结束
+        this.loading = false;
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true;
+        }
+      }, 500);
+    }
   }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.van-list {
+  margin-top: 92px;
+}
 </style>
