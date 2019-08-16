7301955a6c4b03bb0eb5c09d32eccc61dda25022
牛亮 <nll@itcast.cn>
2019-08-16 15:50:49 +0800

    文章详情-点赞和不喜欢实现

--- src/views/article/components/MoreAction.vue
+++ src/views/article/components/MoreAction.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="more-action">
     <van-button
+      @click="handleLike"
       :icon="isLike ? 'star' : 'star-o'"
       round
       :loading="likeLoading"
@@ -8,6 +9,7 @@
     >{{ isLike ? '取消' : '' }}点赞</van-button>
 
     <van-button
+      @click="handleDislike"
       :icon="isDislike ? 'clear' : 'close'"
       round
       :loading="disLikeLoading"
@@ -28,7 +30,6 @@ export default {
   name: 'MoreAction',
   props: ['article'],
   computed: {
-    // 用户对文章的态度, -1: 无态度，0-不喜欢，1-点赞	
     // 是否点赞
     isLike () {
       return this.article.attitude === 1
@@ -42,6 +43,55 @@ export default {
     return {
       likeLoading: false,
       disLikeLoading: false
+    }
+  },
+  methods: {
+    // 点赞
+    async handleLike () {
+      // 判断是否登录
+      if (!this.$checkLogin()) {
+        return
+      }
+      this.likeLoading = true
+      // 用户对文章的态度, -1: 无态度，0-不喜欢，1-点赞	
+      // 判断当前的状态是否是点赞
+      try {
+        if (this.isLike) {
+          // 如果是点赞，取消点赞
+          await unLikesArticle(this.article.art_id)
+          this.article.attitude = -1
+        } else {
+          await likesArticle(this.article.art_id)
+          this.article.attitude = 1
+        }
+      } catch (err) {
+        this.$toast.fail('操作失败' + err)
+      }
+      this.likeLoading = false
+    },
+    // 不喜欢
+    async handleDislike () {
+      // 判断是否登录
+      if (!this.$checkLogin()) {
+        return
+      }
+      this.disLikeLoading = true
+      // 用户对文章的态度, -1: 无态度，0-不喜欢，1-点赞	
+      // 判断当前的状态是否是点赞
+      try {
+        if (this.isDislike) {
+          // 如果是不喜欢，取消不喜欢
+          await unDislikesArticle(this.article.art_id)
+          this.article.attitude = -1
+        } else {
+          // 不喜欢
+          await dislikesArticle(this.article.art_id)
+          this.article.attitude = 0
+        }
+      } catch (err) {
+        this.$toast.fail('操作失败' + err)
+      }
+      this.disLikeLoading = false
     }
   }
 }
