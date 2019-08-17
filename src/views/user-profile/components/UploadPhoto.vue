f1efaf4a239dd5b1d8b7af1d37b99a606b150d06
牛亮 <nll@itcast.cn>
2019-08-17 16:55:48 +0800

    选择图片。图片预览

--- src/views/user-profile/components/UploadPhoto.vue
+++ src/views/user-profile/components/UploadPhoto.vue
@@ -6,7 +6,8 @@
     closeOnClickOverlay
   >
     <van-cell-group>
-      <van-cell title="从相册选择图片" />
+      <van-cell title="从相册选择图片" @click="handleSelectPhoto" />
+      <input type="file" @change="handlePreview" ref="file" style="display: none">
       <van-cell title="拍照" />
       <van-cell title="取消" @click="$emit('input', false)" />
     </van-cell-group>
@@ -14,11 +15,47 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import { ImagePreview } from 'vant'
+
+Vue.use(ImagePreview)
 export default {
   name: 'UploadPhoto',
   props: ['value'],
   data () {
     return {
+    }
+  },
+  methods: {
+    // 点击从相册选择图片，弹出选择文件的对话框
+    handleSelectPhoto () {
+      // 让file点一下，弹出选择文件
+      this.$refs.file.click()
+    },
+    // 当file中选择了文件
+    handlePreview () {
+      // 1. 隐藏当前对话框
+      this.$emit('input', false)
+      // 2. 预览图片
+      ImagePreview({
+        images: [
+          URL.createObjectURL(this.$refs.file.files[0])
+        ],
+        showIndex: false,
+        onClose: this.handleUpload
+      })
+    },
+    // 处理文件上传
+    handleUpload () {
+      // 1. 提示用户是否上传
+      this.$dialog.confirm({
+        message: '是否确认该图片作为头像'
+      }).then(() => {
+        // on confirm
+        // 2. 上传
+      }).catch(() => {
+        // on cancel
+      });
     }
   }
 }
