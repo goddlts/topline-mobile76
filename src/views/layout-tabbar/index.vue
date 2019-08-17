924694a39b3b3b87fa8cfa7647ac8f9c48d3e65d
牛亮 <nll@itcast.cn>
2019-08-17 14:50:48 +0800

    处理我的和未登录文字

--- src/views/layout-tabbar/index.vue
+++ src/views/layout-tabbar/index.vue
@@ -6,13 +6,20 @@
       <van-tabbar-item icon="home-o" replace to="/">主页</van-tabbar-item>
       <van-tabbar-item icon="search" to="/qa">问答</van-tabbar-item>
       <van-tabbar-item icon="friends-o" to="/video">视频</van-tabbar-item>
-      <van-tabbar-item icon="setting-o" to="/my">我的</van-tabbar-item>
+      <van-tabbar-item icon="setting-o" to="/my">{{ user ? '我的' : '未登录' }}</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
+  computed: {
+    // user () {
+    //   return this.$store.state.user
+    // }
+    ...mapState(['user'])
+  },
   data () {
     return {
       active: 0
